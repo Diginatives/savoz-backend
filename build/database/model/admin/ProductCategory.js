d129ceb75c777514be779ea88d142a28d0dd98c3
Kashifdeveloper06 <Kashifdeveloper06>
2023-07-07 10:42:56 +0500

    build 2.5

---
+++ build/database/model/admin/ProductCategory.js
@@ -0,0 +1,16 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.PRODUCT_CATEGORIES_COL = exports.PRODUCT_CATEGORIES_TABLE_NAME = void 0;
+exports.PRODUCT_CATEGORIES_TABLE_NAME = 'productCategories';
+exports.PRODUCT_CATEGORIES_COL = {
+    id: 'id',
+    subCategoryId: 'subCategoryId',
+    name: 'name',
+    description: 'description',
+    image: 'image',
+    isDeleted: 'isDeleted',
+    isActive: 'isActive',
+    createdAt: 'createdAt',
+    updatedAt: 'updatedAt',
+};
+//# sourceMappingURL=ProductCategory.js.map
