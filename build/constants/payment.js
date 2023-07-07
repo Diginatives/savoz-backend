a2bf8853f694b871125b188df142807b96d9d6c4
Kashifdeveloper06 <Kashifdeveloper06>
2023-07-07 09:41:26 +0500

    build 2

--- build/constants/payment.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var PAYMENT_METHOD = {
-    CREDITCARD: 'cc',
-    BANKACCOUNT: 'ba',
-};
-exports.default = PAYMENT_METHOD;
-//# sourceMappingURL=payment.js.map
