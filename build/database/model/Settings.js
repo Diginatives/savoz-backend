d5c7d47a2efd0cc85d51fd3e3107cf0fea9ef3cd
Kashifdeveloper06 <Kashifdeveloper06>
2023-07-12 11:48:43 +0500

    changes in packages and build

--- build/database/model/Settings.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.SETTINGS_COL = exports.SETTINGS_TABLE_NAME = void 0;
-exports.SETTINGS_TABLE_NAME = 'settings';
-exports.SETTINGS_COL = {
-    id: 'id',
-    minOrderLimit: 'minOrderLimit',
-};
-//# sourceMappingURL=Settings.js.map
